d38d783972aacebfe6500518db8f889416b2bfc9
JCheraparambil <JCheraparambil@IS197.dandh.net>
2021-10-21 14:41:56 -0400

    test

--- test-rest-api/src/main/java/com/jx/restapi/TestRestApp.java
+++ test-rest-api/src/main/java/com/jx/restapi/TestRestApp.java
@@ -3,6 +3,7 @@ package com.jx.restapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//spring initiations
 @SpringBootApplication
 public class TestRestApp {
   public static void main(String[] args) {
